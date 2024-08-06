4079d8b4dd0c5db045f72cd1ada025f372662560
Yoav Vaknin <yoav842v@gmail.com>
2024-08-06 21:29:46 +0300

    Changed script to create CSV with user details specified according to our format, and recreated the CSV.

--- select_fabricated_users_data.py
+++ select_fabricated_users_data.py
@@ -1,8 +1,33 @@
 import pandas as pd
 
+USER_DETAILS = '''
+Name: %s
+Gender: %s
+Date of birth: %s
+Interests: %s
+City of residence: %s
+Country: %s
+'''
+
 # Reading the fabricated users dataset
-users_dataset = pd.read_csv('data/fabricated_users/SocialMediaUsersDataset.csv')
-# Selecting 100 fabricated users data
+users_dataset = pd.read_csv(
+    'data/fabricated_users/SocialMediaUsersDataset.csv')
+
+# Selecting 100 fabricated users data, processing to string
 randomly_selected_users = users_dataset.sample(100)
-# Saving data to a CSV
-randomly_selected_users.to_csv('data/fabricated_users/RandomlySampledUsers.csv')
+user_details = [
+    USER_DETAILS % (
+        user.Name,
+        user.Gender,
+        user.DOB,
+        user.Interests,
+        user.City,
+        user.Country
+    )
+    for _, user in randomly_selected_users.iterrows()
+]
+user_details_df = pd.DataFrame(user_details, columns=['user_details'])
+
+# Saving dataframes to CSV
+user_details_df.to_csv(
+    'data/fabricated_users/RandomlySampledUsers.csv')
